424a77c7f9b073d0c0d06698f70cb7cec001c5f1
Shahid <abdul.baset@aci-bd.com>
2019-01-03 17:57:46 +0600

    2nd commit service example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,9 +1,23 @@
+import { CoursesService } from './courses.service';
 import {Component} from '@angular/core'
 
 @Component({
     selector:'courses',
-    template: '<h2>Courses</h2>'
+    template: `<h2>{{ getTitle() }}</h2>
+    <ul>
+        <li *ngFor='let course of courses'>
+            {{course}}
+        </li>
+    </ul>`
 })
 export class CoursesComponent{
-
+    title:string = "List Of Course";
+    courses;
+    getTitle(){
+        return this.title;
+    }
+    constructor(){
+        let servics = new CoursesService();
+        this.courses = servics.getCourses();
+    }
 }
