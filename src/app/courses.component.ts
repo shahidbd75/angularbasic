69204bf8179f5daf5172f3e8794df6d2bb630907
Shahid Ab <shahidab@live.com>
2019-01-05 23:01:03 +0600

    style binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,32 @@ import {Component} from '@angular/core'
 @Component({
     selector:'courses',
     template: `<h2>{{ getTitle() }}</h2>
+    <img src={{imageUrl}} />
+    <img [src]="imageUrl" />
     <ul>
         <li *ngFor='let course of courses'>
             {{course}}
         </li>
-    </ul>`
+    </ul>
+    <button class="btn btn-primary">Button</button>
+    <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
+    <button [style.backgroundColor]="isActive?'green':'white'">Style Binding</button>
+    <table>
+        <tr>
+            <td [attr.colSpan]="colSpan"></td>
+        </tr>
+    </table>`
 })
 export class CoursesComponent{
     title:string = "List Of Course";
+    imageUrl = "http://lorempixel.com/400/200/";
+    colSpan =2;
+    isActive=false;
     courses;
     getTitle(){
         return this.title;
     }
-    constructor(){
-        let servics = new CoursesService();
-        this.courses = servics.getCourses();
+    constructor(courseService:CoursesService){
+        this.courses = courseService.getCourses();
     }
 }
