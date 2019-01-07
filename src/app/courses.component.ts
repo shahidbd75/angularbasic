70da03666a0b4c0a43f1726f6935a58d17960fec
Shahid <abdul.baset@aci-bd.com>
2019-01-07 17:24:00 +0600

    template variable

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,21 +4,48 @@ import {Component} from '@angular/core'
 @Component({
     selector:'courses',
     template: `<h2>{{ getTitle() }}</h2>
-    <img src={{imageUrl}} />
+    <img src={{imageUrl}} /><br/>
     <img [src]="imageUrl" />
     <ul>
         <li *ngFor='let course of courses'>
             {{course}}
         </li>
     </ul>
-    <button class="btn btn-primary">Button</button>
-    <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
+    <button class="btn btn-primary">Button</button><br/>
+    <button class="btn btn-primary" [class.active]="isActive">Class Binding</button><br/>
     <button [style.backgroundColor]="isActive?'green':'white'">Style Binding</button>
-    <table>
-        <tr>
-            <td [attr.colSpan]="colSpan"></td>
-        </tr>
-    </table>`
+    <div class="row">
+    <div class="col-lg-6">
+        <table>
+            <tr>
+                <td [attr.colSpan]="colSpan">
+                    <input type='text' class='form-control' (keyup)='onKeyUp($event)' />
+                    <input type='text' class='form-control' id='ku' (keyup.enter)='onEnterKeyUp()' />
+                </td>
+            </tr>
+            <tr>
+            <td>Two way binding</td>
+                <td>
+                    <input [value]='mail' (keyup.enter)='onKeyUpEmailAddr($event.target.value)'/>
+                </td>
+            </tr>
+            <tr>
+            <td>Banana in Box</td>
+                <td>
+                    <input ([ngModel])='fname' class='form-control' (keyup.enter)='getFirstName()'/>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="col-lg-6" (click)="onDivClick()">
+        <button class="btn btn-success" (click)='onButtonClick($event)'>Click Me!!</button>
+    </div>
+    </div>
+    <div>
+        Template Variable
+        : <input #email class='form-control' (keyup.enter)='onKeyUpEmail(email.value)' />
+    </div>
+    `
 })
 export class CoursesComponent{
     title:string = "List Of Course";
@@ -26,10 +53,37 @@ export class CoursesComponent{
     colSpan =2;
     isActive=false;
     courses;
+    mail='demo@ymail.com';
+    fname;
     getTitle(){
         return this.title;
     }
     constructor(courseService:CoursesService){
         this.courses = courseService.getCourses();
+    }
+    onButtonClick($event){
+        console.log($event);
+        $event.stopPropagation();
+        console.log('button was clicked');
+    }
+    onDivClick(){
+        console.log('div was clicked');
+    }
+    onKeyUp ($event){
+        if($event.keyCode==13){
+            console.log('Enter was pressed');
+        }
+    }
+    onEnterKeyUp(){
+        console.log('ENTER key pressed');
+    }
+    onKeyUpEmail(email){
+        console.log(email);
+    }
+    onKeyUpEmailAddr(value){
+        console.log(value);
+    }
+    getFirstName(){
+        console.log(this.fname);
     }
 }
--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -1,5 +1,10 @@
 export class CoursesService {
     getCourses(){
-        return ["course 1","course 2","course 3"];
+        return ["asp.net core","asp.net web api","Complete Angular"];
+        // return [
+        //     {name:"asp.net core",author:"Deborah Qurata",rating:3.5,publishedDate:new Date(2016,11,09)},
+        //     {name:"asp.net web api",author:"Scoot Allen",rating:4.0,publishedDate:new Date(2016,11,19)},
+        //     {name:"Complete Angular",author:"Mosh Hamdani",rating:4.5,publishedDate:new Date(2017,5,10)}
+        // ];
     }
 }
